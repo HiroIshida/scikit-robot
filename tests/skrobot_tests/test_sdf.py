5273e01243d50388215dae15acf73054a0a3da74
HiroIshida <spitfire.docomo@gmail.com>
2020-11-15 09:24:27 +0900

    add transform test

--- tests/skrobot_tests/test_sdf.py
+++ tests/skrobot_tests/test_sdf.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import copy
 import numpy as np
 from numpy import testing
 import trimesh
@@ -20,7 +21,7 @@ class TestSDF(unittest.TestCase):
         cls.boxsdf = skrobot.sdf.BoxSDF([0, 0, 0], [0.005, 0.01, 0.1])
 
     def test_box__signed_distance(self):
-        sdf = self.boxsdf 
+        sdf = copy.deepcopy(self.boxsdf)
         X_origin = np.zeros((1, 3))
         self.assertEqual(sdf._signed_distance(X_origin), -0.0025)
 
@@ -31,8 +32,26 @@ class TestSDF(unittest.TestCase):
         testing.assert_array_equal(sdf._signed_distance(X_edges), [0, 0])
 
     def test_box__surface_points(self):
-        N = 20
-        ray_tips, sd_vals = sdf._surface_points(N=N)
+        sdf = copy.deepcopy(self.boxsdf)
+        ray_tips, sd_vals = sdf._surface_points()
         testing.assert_array_equal(sdf._signed_distance(ray_tips), sd_vals)
         assert np.all(np.abs(sd_vals) < sdf._surface_threshold)
 
+    def test_transform_pts_obj_to_sdf_and_sdf_to_obj(self):
+        # transform_pts_obj_to_sdf and transform_pts_sdf_to_obj
+        sdf = copy.deepcopy(self.boxsdf)
+        boxmodel = skrobot.model.Link()
+        boxmodel.assoc(sdf)
+        translation = np.array([0.1, 0.1, 0.1])
+        boxmodel.translate(translation)
+        points_obj = np.random.randn(100, 3)
+        points_sdf = sdf.transform_pts_obj_to_sdf(points_obj.T)
+
+        # test transform_pts_obj_to_sdf
+        points_sdf_should_be = points_obj - \
+                np.repeat(translation.reshape((1, -1)), 100, axis=0)
+        testing.assert_array_almost_equal(points_sdf, points_sdf_should_be)
+
+        # test transform_pts_sdf_to_obj
+        points_obj_recreated = sdf.transform_pts_sdf_to_obj(points_sdf.T)
+        testing.assert_array_almost_equal(points_obj_recreated, points_obj)
