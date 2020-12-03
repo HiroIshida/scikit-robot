e24a41b15e742f86c6ae46144d2767f056a2799e
HiroIshida <spitfire.docomo@gmail.com>
2020-12-03 23:06:55 +0900

    add test for union_sdf

--- tests/skrobot_tests/test_sdf.py
+++ tests/skrobot_tests/test_sdf.py
@@ -1,5 +1,5 @@
 # The testing order is 
-# BoxSDF -> SignedDistanceFunction -> GridSDF
+# BoxSDF -> SignedDistanceFunction -> GridSDF > UnionSDF
 
 import unittest
 import os
@@ -25,35 +25,36 @@ class TestSDF(unittest.TestCase):
         cls.bunnymesh = bunnymesh
 
         # prepare boxsdf and boxmodel
-        boxsdf = skrobot.sdf.BoxSDF([0, 0, 0], [0.005, 0.01, 0.1])
-        boxmodel = skrobot.model.Link()
-        boxmodel.assoc(boxsdf)
-        boxtrans = np.array([0.1, 0.1, 0.1])
-        boxmodel.translate(boxtrans)
+        box_withds = np.array([0.05, 0.1, 0.05])
+        boxsdf = skrobot.sdf.BoxSDF([0, 0, 0], box_withds)
+        boxtrans = np.array([0.0, 0.1, 0.0])
+        boxsdf.translate(boxtrans)
 
+        cls.box_withds = box_withds
         cls.boxsdf = boxsdf
-        cls.boxmodel = boxmodel
         cls.boxtrans = boxtrans
-        cls.points_box_edge_sdf = np.array([
-            [-0.0025, -0.005, -0.02],
-            [-0.0025, -0.005, 0.02],
-            ])
+        cls.points_box_edge_sdf = np.array(
+               [-0.5 * box_withds, 0.5 * box_withds])
+
+        # preapare UnionSDF
+        unionsdf = skrobot.sdf.UnionSDF(sdf_list=[boxsdf, gridsdf])
+        cls.unionsdf = unionsdf
 
     def test_box__signed_distance(self):
-        sdf, model = self.boxsdf, self.boxmodel
+        sdf = self.boxsdf
         X_origin = np.zeros((1, 3))
-        self.assertEqual(sdf._signed_distance(X_origin), -0.0025)
+        self.assertEqual(sdf._signed_distance(X_origin), -min(self.box_withds) * 0.5)
         testing.assert_array_equal(
                 sdf._signed_distance(self.points_box_edge_sdf), [0, 0])
 
     def test_box__surface_points(self):
-        sdf, model = self.boxsdf, self.boxmodel
+        sdf = self.boxsdf
         ray_tips, sd_vals = sdf._surface_points()
         testing.assert_array_equal(sdf._signed_distance(ray_tips), sd_vals)
         assert np.all(np.abs(sd_vals) < sdf._surface_threshold)
 
     def test__transform_pts_obj_to_sdf_and_sdf_to_obj(self):
-        sdf, model, trans = self.boxsdf, self.boxmodel, self.boxtrans
+        sdf, trans = self.boxsdf, self.boxtrans
         points_obj = np.random.randn(100, 3)
         points_sdf = sdf.transform_pts_obj_to_sdf(points_obj)
 
@@ -67,20 +68,20 @@ class TestSDF(unittest.TestCase):
         testing.assert_array_almost_equal(points_obj_recreated, points_obj)
 
     def test___call__(self):
-        sdf, model, trans = self.boxsdf, self.boxmodel, self.boxtrans
+        sdf, trans = self.boxsdf, self.boxtrans
         points_box_edge_obj = np.array(
                 [x + trans for x in self.points_box_edge_sdf])
         testing.assert_array_almost_equal(
                 sdf(points_box_edge_obj), [0, 0])
 
     def test_surface_points(self):
-        sdf, model = self.boxsdf, self.boxmodel
+        sdf = self.boxsdf
         surface_points_obj, _ = sdf.surface_points(n_sample=20)
         sdf_vals = sdf(surface_points_obj)
         assert np.all(np.abs(sdf_vals) < sdf._surface_threshold)
 
     def test_on_surface(self):
-        sdf, model = self.boxsdf, self.boxmodel
+        sdf = self.boxsdf
         points_box_edge_obj = sdf.transform_pts_sdf_to_obj(self.points_box_edge_sdf)
         logicals_positive, _ = sdf.on_surface(points_box_edge_obj)
         assert np.all(logicals_positive) 
@@ -123,3 +124,30 @@ class TestSDF(unittest.TestCase):
         surf_points_obj, _ = sdf.surface_points()
         logicals, _ = sdf.on_surface(surf_points_obj)
         assert np.all(logicals) 
+
+    def test_unionsdf___call__(self):
+        sdf = self.unionsdf
+        pts_on_surface = np.array([
+            [-0.07196818,  0.16532058, -0.04285806],
+            [ 0.02802324,  0.11360088, -0.00837826],
+            [-0.05472828,  0.03257335,  0.00886164],
+            [ 0.0077233 ,  0.15      , -0.01742908],
+            [ 0.02802324,  0.11360088, -0.00837826],
+            [-0.07714015,  0.15152866,  0.0329975 ]
+            ])
+        assert np.all(abs(sdf(pts_on_surface)) < sdf.surface_threshold)
+
+    def test_unionsdf_surface_points(self):
+        sdf = self.unionsdf
+        sdf.surface_points()
+        pts, sd_vals = sdf.surface_points()
+        assert np.all(np.abs(sd_vals) < sdf.surface_threshold)
+
+        sub_sdf1, sub_sdf2 = self.unionsdf.sdf_list
+        on_surface1 = sub_sdf1(pts) < sub_sdf1.surface_threshold
+        on_surface2 = sub_sdf2(pts) < sub_sdf2.surface_threshold
+        cond_or = np.logical_or(on_surface1, on_surface2) 
+        assert np.all(cond_or) # at least on either of the surface
+
+        cond_and = (sum(on_surface1) > 0) and (sum(on_surface2) > 0)
+        assert cond_and # each surface has at least a single points
